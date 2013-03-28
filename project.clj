4e6a2ea910aba446a8ef021a27a5a9dc57441262
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-03-28 09:31:51 -0400

    Updated to Clojure 1.4.0. Bumped version to 1.3.2.

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject propertea "1.3.1"
+(defproject cc.artifice/propertea "1.3.2"
   :description "load, coerce, and validate property files"
-  :jar-name "propertea.jar"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[expectations "1.0.0"]
-                     [lein-expectations "0.0.1"]
-                     [lein-publishers "1.0.4"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [expectations "1.4.33"]]
+  :plugins [[lein-expectations "0.0.7"]])
